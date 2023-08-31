d0d3c89001af8d03f8cf7a17f00720371eaf4284
MichielMe <michiel.meire@hotmail.com>
2023-08-31 14:15:34 +0200

    updated transcript

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import openai
 from pytube import YouTube
 from dotenv import load_dotenv
 from pydub import AudioSegment
+import subprocess
 
 load_dotenv()
 
@@ -10,15 +11,37 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 openai.api_key = api_key
 
-video_url = "https://www.youtube.com/watch?v=cdiD-9MMpb0&t=10378s"
+video_url = "https://www.youtube.com/watch?v=5xbADDvciko"
 yt = YouTube(video_url)
 audio_stream = yt.streams.filter(only_audio=True).first()
-audio_stream.download(filename="audio")
+audio_stream.download(filename="audio.webm")
 
-with open("audio", "rb") as audio_file:
-    transcript = openai.Audio.transcribe(model="whisper-1", file=audio_file)
-transcribed_text = transcript['data']['text']
+# Convert the audio to WAV format using FFmpeg
+subprocess.run(["ffmpeg", "-i", "audio.webm", "audio.wav"])
 
-print("Transcibed Text:", transcribed_text)
-with open("transcript.txt", "w") as text_file:
-    text_file.write(transcribed_text)
+# Step 2: Check and Split Audio if Necessary
+audio = AudioSegment.from_file("audio.wav", format="wav")
+
+
+chunk_length = 2 * 60 * 1000  # 10 minutes in milliseconds
+# Step 4: Initialize Transcript
+full_transcript = ""
+
+for i in range(0, len(audio), chunk_length):
+    chunk = audio[i:i+chunk_length]
+    chunk.export("audio_chunk.wav", format="wav")
+    
+    with open("audio_chunk.wav", "rb") as audio_file:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        
+    # Using the actual structure of the transcript object based on OpenAI's API response
+    transcribed_text = transcript['text']
+    
+    full_transcript += transcribed_text + " "
+
+# Optional: Save the full transcript to a text file
+with open("full_transcript.txt", "w") as f:
+    f.write(full_transcript)
+
+# Done!
+print("Transcription Complete. The full transcript has been saved to 'full_transcript.txt'.")
